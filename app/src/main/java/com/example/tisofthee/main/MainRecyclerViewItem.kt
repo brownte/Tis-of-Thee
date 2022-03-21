6f2821031276a6aafb39d753fd3f8a4407f9589d
T <telbrown@gmail.com>
2022-03-20 20:14:07 -0500

    Revert "This is my latest commit. My project is still a major work in progress. I still need to refactor my code to follow the MVP design pattern, but the bones are there - I just ran out of time before the due date."
    This reverts commit 1e91faee2c238534c7d817f1cbad108218f2e969.

--- app/src/main/java/com/example/tisofthee/main/MainRecyclerViewItem.kt
+++
@@ -1,4 +0,0 @@
-package com.example.tisofthee.main
-//TODO: code later after refactoring
-
-data class MainRecyclerViewItem(val flagText: String, val countryNameText: String, val countryCodeText: String)
--- app/src/main/java/com/example/tisofthee/results/ResultsActivityView.kt
+++
@@ -1,4 +0,0 @@
-package com.example.tisofthee.results
-
-interface ResultsActivityView {
-}
